71b0aea396c3631e5118c7ab90c212e82874364b
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-01-24 10:06:54 -0800

    started by writing out some directions in js file commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//create the time blocks
+//color coding, past, present, or future
+//when click, enter event
+//click to save to local storage
+
+//time for current date 
+//loop for rows and columns
+//rows
+//columns
+
+//append
+
+//save button
+
+//clear button
+
