faa337d5c9fd384db22898870e0b20c10ae952a4
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-01-26 06:15:35 -0800

    updated time, converted with moment, trying to look at the colors on js commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,23 +3,55 @@ $(".clear-btn").text("Clear!");
 
 $( document ).ready(function() {
 
+ 
     //using moment for the date
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    //current time
+    var currentT = new Date().getHours();
+    currentT = moment(currentT, 'HH:mm:ss').format('h A');
+    console.log(currentT);
+
+   
+
+
+
+
 
     //loop to put rows and columns, i is = to 9 rows.
     for(var i = 9; i < 18; i++){
 
         //row 1
-        var row = $('<div class="row border border-primary">')
+        var row = $("<article>");
+        row.addClass("row");
 
         //column 1, where time will go
-        var column1 = $('<div class="col-2 border border-danger">');
+        var column1 = $("<article>");
+        column1.addClass("col-2 time");
+        //adds time to the first rows
+        var timeShown = $("<p>");
+        timeShown.addClass("hour");
+        timeShown.text(i);
+        if(i < 12) {
+            timeShown.text(i + " AM");
+        } else if(i == 12) {
+            timeShown.text(i + " PM");
+        } else {
+            timeShown.text(i - 12 + " PM");
+        }
+        column1.append(timeShown);
+
 
         //column 2, where they write event
-        var column2 = $('<div class="col-8 border border-dark">');
+        var column2 = $("<input>");
+        column2.addClass("col-8 textArea");
 
         //column3, save button
-        var column3 = $('<div class="col-2 border border-danger">');
+        var column3 = $("<article>");
+        column3.addClass("col-1 save-button")
+        var button = $("<button>");
+        button.addClass("saveBtn save");
+        column3.append(button);
+        //var column3 = $(`<div class="col-1"> <button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`);
 
         //append each column to a row
         row.append(column1);
@@ -29,9 +61,16 @@ $( document ).ready(function() {
         //appends the row to the container class in the html
         $(".container").append(row);
         console.log("hi");
-
+        
     }
 
+    
     //creating save button
-    var saveBtn = $("")
+    $(".saveBtn").text("save");
+
+   
+    //colors
+  
+    
+    
 })
